0b87db4e27f294132e2dd987a29299eb7bbf9304
kurgeri <b3kurger@vasvari.org>
2026-01-26 12:57:02 +0100

    Naptár, Menü

---
+++ naptar_ikt_feladat/naptar_ikt_feladat/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace naptar_ikt_feladat
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Menu();
+           
+        }
+
+        static void Menu()
+        {
+            Console.Write($"N: Naptár\nÚ: Új esemény rögzítése\nL : Legközelebbi esemény\n K: Kilépés\nVálasszon a kívánt opciók közül: ");
+            string opcio = Console.ReadLine().ToUpper();
+   
+
+            switch(opcio)
+            {
+                case "N":
+                    Console.Clear();
+                    Naptar();
+                    break;
+                case "Ú":
+                    // Új események függvény
+                    break;
+                case "L":
+                    // Legközelebbi esemény függvényg
+                    break;
+                case "K":
+                    // Kilépés függvény? (terminálni kell a számítót)
+                    break;
+
+
+
+            }
+
+            
+        }
+        static void Naptar()
+        {
+            string[,] naptar = new string[5, 7];
+            int napok = 0;
+            for (int i = 0; i < naptar.GetLength(0); i++)
+            {
+             
+                for (int j = 0; j < naptar.GetLength(1); j++)
+                {
+                    napok++;
+                    naptar[i, j] = Convert.ToString(napok);
+                    if(napok > 29)
+                    {
+                        naptar[i, j] = String.Empty;
+                    }
+                    
+
+
+                    Console.Write($"{naptar[i,j]} ");
+                }
+                Console.WriteLine();
+            }
+        }
+
+
+    }
+}
---
+++ naptar_ikt_feladat/naptar_ikt_feladat/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("naptar_ikt_feladat")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("naptar_ikt_feladat")]
+[assembly: AssemblyCopyright("Copyright ©  2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("fbfe2b43-1ec7-43c4-805e-b8776aed3e59")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
